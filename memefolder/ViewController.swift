f745addf7d8fa413ae9d09cfcb02d8c86a32469f
elliotfiske <elliotfiske@gmail.com>
2020-01-03 14:02:03 -0800

    Initial Commit

---
+++ memefolder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memefolder
+//
+//  Created by Elliot Fiske on 1/3/20.
+//  Copyright © 2020 Meme Folder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
