660620d8ff2c3421c0893f7a0a9c72b44e61897a
elliotfiske <elliotfiske@gmail.com>
2020-01-04 20:10:12 -0800

    Successfully moved from cocoapods to Swift Packages, added PromiseKit

--- Pods/Target Support Files/Pods-YoutubeDL/Pods-YoutubeDL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YoutubeDL : NSObject
-@end
-@implementation PodsDummy_Pods_YoutubeDL
-@end
--- YoutubeDL/YoutubeDL/twitter/TwitterDL.swift
+++ YoutubeDL/YoutubeDL/twitter/TwitterDL.swift
@@ -1,31 +1,47 @@
 //
 //  TwitterDL.swift
-//  YoutubeDL
+//  youtubedlswift
 //
 //  Created by Elliot Fiske on 1/4/20.
 //  Copyright © 2020 Meme Folder. All rights reserved.
 //
 
 import Foundation
+import PromiseKit
+import PMKAlamofire
 
 public class TwitterDL {
    static let BEARER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAAPYXBAAAAAAACLXUNDekMxqa8h%2F40K4moUkGsoc%3DTYfbDKbT3jJPCEVnMYqilB28NHfOPqkca3qaAxGfsyKCs0wRbw"
    
-   static let API_BASE = "https://api.twitter.com/1.1/"
+   static let BASE_API = "https://api.twitter.com/1.1/"
    
    // We will need this guest token to access Twitter as a guest, until
    //    Twitter approves my developer account
-   static var guestToken: String?
+   static func getGuestToken() -> Promise<String> {
+      let headers: HTTPHeaders = [
+         "Authorization" : BEARER_TOKEN,
+         "Accept" : "application/json"
+      ]
+      
+      return Alamofire.request(BASE_API + "guest/activate.json",
+                               method: .post,
+                               headers: headers)
+         .responseJSON()
+         .map { json, _ -> String in
+            print(json)
+            return "test"
+         }
+   }
    
    public static func callAPI() {
-      if guestToken == nil {
-         let tokenURL = URL(string: API_BASE + "guest/activate.json")!
-         let headers = [
-            "Authorization" : BEARER_TOKEN,
-            "Accept" : "application/json"
-         ]
-         
-         
+      var guestToken: String?
+      
+      firstly {
+         getGuestToken()
+      }
+      .done { guestToken = $0 }
+      .catch { error in
+         print(error.localizedDescription)
       }
    }
 }
