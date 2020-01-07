eb44734645dab36804909021f77f6cf47b34c9d4
mlinsenbard <m.linsenbard@gmail.com>
2020-01-07 01:35:10 -0800

    Give up on trying to animate constraints
    womp

--- memefolder/ViewController+ErrorHandler.swift
+++ memefolder/ViewController+ErrorHandler.swift
@@ -18,14 +18,12 @@ extension ViewController {
             view.sendSubviewToBack(errorLabel)
 
             // Animate the error label to pop up from behind TextView and fade in.
-            errorLabelBottomConstraint.constant = 8
             errorLabel.alpha = 0
             errorLabel.isHidden = false
 
-            UIView.animateKeyframes(withDuration: 0.32, delay: 0, options: [], animations: {
+            UIView.animateKeyframes(withDuration: 0.35, delay: 0, options: [], animations: {
                 UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.32) { [weak self] in
                     self?.errorLabel.alpha = 1
-                    self?.errorLabel.setNeedsLayout()
                 }
             })
         }
@@ -33,15 +31,12 @@ extension ViewController {
 
     func hideInputError() {
         guard !errorLabel.isHidden else { return }
-        errorLabelBottomConstraint.constant = -20
 
-        UIView.animate(withDuration: 0.24, delay: 0, options: .curveEaseOut, animations: { [weak self] in
-            self?.errorLabel.alpha = 0
-            self?.errorLabel.setNeedsLayout()
-        }) { [weak self] complete in
-            if complete {
-                self?.errorLabel.isHidden = true
-            }
-        }
+        UIView.animateKeyframes(withDuration: 0.4, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6, animations: { [weak self] in
+                self?.errorLabel.alpha = 0
+            })}, completion: { _ in
+                self.errorLabel.isHidden = true
+        })
     }
 }
